7a3a7b3a902b44a197bad8601b1ee3b0dd15f613
Subina Chhetri <120252788+subuchhetri34@users.noreply.github.com>
2024-07-24 16:24:17 +0545

    webapplication to upload a file and capture traffic while uploading a file

---
+++ Assignment-8/web-application/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+# Configurations
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'File successfully uploaded'
+    else:
+        return 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'
+
+if __name__ == '__main__':
+    app.run(debug=True)
